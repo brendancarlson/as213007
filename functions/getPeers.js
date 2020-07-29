0854e0e8d04751224eb3d9dfabe0e5e9ee382e26
Brendan Carlson <brendan@bcarlsonmedia.com>
2020-07-28 17:43:05 -0700

    Dark mode fixes

--- functions/getPeers.js
+++ functions/getPeers.js
@@ -1,9 +1,11 @@
 /* eslint react/no-unknown-property: 0 */
 /* eslint react/prefer-stateless-function: 0 */
 import React, { useState, useEffect } from "react";
+import { useColorMode } from "theme-ui"
 
 const GetPeers = () => {
   const [data, setData] = useState([]);
+  const [colorMode, setColorMode] = useColorMode()
 
   useEffect(() => {
     // get data from RIPE
@@ -19,7 +21,7 @@ const GetPeers = () => {
 
   return (
     <div>
-      <p>{data.map(neighbour => <a href={`https://bgp.he.net/AS${neighbour.asn}`}>AS{neighbour.asn}</a>)}</p>
+      <p>{data.map(neighbour => <a className={colorMode === 'light' ? 'a-light' : 'a-dark'} href={`https://bgp.he.net/AS${neighbour.asn}`} alt={`AS${neighbour.asn} Link to BGP.HE.NET`} target="_blank">AS{neighbour.asn}</a>)}</p>
     </div>
   );
 };
